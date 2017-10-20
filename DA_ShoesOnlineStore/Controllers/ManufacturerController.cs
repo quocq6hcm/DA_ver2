7d53c8679e03a367f683f1980c71bcdf5ce66336
Nguyễn Gia Quốc <quocq6hcm@hotmail.com>
2017-10-20 17:11:17 +0700

    update database model

---
+++ DA_ShoesOnlineStore/Controllers/ManufacturerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace DA_ShoesOnlineStore.Controllers
+{
+    public class ManufacturerController : Controller
+    {
+
+
+        Models.SHOESWebSiteEntities _db = new Models.SHOESWebSiteEntities();
+        // GET: Manufacturer
+        public ActionResult Index()
+        { 
+            var model = _db.NHACUNGCAPs.ToList();
+            return View(model);
+        }
+    }
+}
