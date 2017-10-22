6b4c88fcc6c406604d70478e4342da7e07bb95bb
Nguyễn Gia Quốc <quocq6hcm@hotmail.com>
2017-10-22 20:57:01 +0700

    Đăng nhập, đăng ký, index admin

--- DA_ShoesOnlineStore/Controllers/HomeController.cs
+++ DA_ShoesOnlineStore/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace DA_ShoesOnlineStore.Controllers
 {
     public class HomeController : Controller
     {
+        Repositories.UserDAO userDAO = new Repositories.UserDAO();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +26,70 @@ namespace DA_ShoesOnlineStore.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Register() => View();
+
+        [HttpPost]
+        public ActionResult Register(Models.NguoiDung newUser)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                { 
+                    userDAO.AddUser(newUser);
+                    ModelState.AddModelError("", "Thêm người dùng thành công");
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("AddError", "Thêm người dùng thất bại");
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("AddError","Thêm người dùng thất bại");
+                return View();
+
+            }
+           
+            
+        }
+
+        public ActionResult UserNotFound()
+        {
+            return View("LoginError");
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.NguoiDung loginUser)
+        {
+            try
+            {
+                var check = userDAO.LoginUser(loginUser);
+                if (check == null)
+                {
+                    return RedirectToAction("UserNotFound");
+                }
+                else
+                {
+                    
+                    return RedirectToAction("Index");
+                  
+                }
+            }
+            catch (Exception)
+            { 
+                return RedirectToAction("UserNotFound");
+            }
+           
+        }
+
+        public ActionResult LogOut()
+        {
+            userDAO.Logout();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ DA_ShoesOnlineStore/Repositories/UserDAO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DA_ShoesOnlineStore.Repositories
+{
+    public class UserDAO
+    {
+        Models.SHOESWebSiteEntities _db = new Models.SHOESWebSiteEntities();
+
+        public void AddUser(Models.NguoiDung newUser)
+        {
+            newUser.role = 0;
+            newUser.NamSinh = null;
+
+            _db.NguoiDungs.Add(newUser);
+            _db.SaveChanges();
+            return;
+        }
+
+        public Models.NguoiDung LoginUser(Models.NguoiDung loginUser)
+        {
+            var u = _db.NguoiDungs.SingleOrDefault(user => user.TaiKhoan.Equals(loginUser.TaiKhoan));
+            if (u != null)
+            {
+                if (u.MatKhau.Equals(loginUser.MatKhau))
+                {
+                    HttpContext.Current.Session["User"] = u.HoTen;
+                    return u;
+                }
+                    
+            }
+            return null;
+        }
+
+        public void Logout()
+        {
+            HttpContext.Current.Session["User"] = null;
+        }
+    }
+}
